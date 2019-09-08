87084452aa8f6b63561468b38cfef808c9d7126a
Huguenin Yves (KSW Switzerland) <Yves.Huguenin@kswtech.ch>
2019-09-08 11:08:53 +0200

    test

--- vr22.py
+++ vr22.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+#####################################################################################################
+#	Main class ( gestion) for the app																																								#
+#	Was the first writing in no class, don the main loop and init all the stuff																													#
+#	Creator:		Yves Huguenin																																									#
+#	Date:			06.09.2019																																										#
+#	Version:		0.1																																													#
+#																																																		#
+######################################################################################################!/usr/bin/env python3
 from PyQt5 					import	uic
 from PyQt5.QtGui			import	QImage ,  QPixmap
 from PyQt5.QtWidgets 	import 	QMessageBox
