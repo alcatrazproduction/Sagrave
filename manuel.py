738b0aa33f0d90cee2a89e2627bc6d104990cba0
Huguenin Yves (KSW Switzerland) <Yves.Huguenin@kswtech.ch>
2019-10-23 20:39:23 +0200

    ***

---
+++ manuel.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#######################################################################################################
+#	Main class ( gestion) for the app																																									#
+#	Was the first writing in no class, don the main loop and init all the stuff																														#
+#	Creator:		Yves Huguenin																																										#
+#	Date:			06.09.2019																																											#
+#	Version:		0.1																																														#
+#																																																			#
+#######################################################################################################
